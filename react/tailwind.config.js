33b89bb54f3d4160e9a8e18c4679add669356a97
Lindsey Verbauwhede <mora376@hotmail.com>
2024-09-18 16:02:34 +0200

    First Commit

---
+++ react/tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+ import forms from '@tailwindcss/forms';
+
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+     forms,
+
+  ],
+};
